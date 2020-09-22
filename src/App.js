ccc4619b8f9f1767af38b75a3cf3d1cc14de372d
William Dieter <williamzdieter@gmail.com>
2020-09-22 01:46:35 -0300

    XABLAU

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    };
+    this.fetchDog = this.fetchDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  fetchDog() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(result => this.setState({ data: result }));
+  }
+
+  render() {
+    if (this.state.data === "") return "loading...";
+    return (
+      <div>
+        <p>Doguinhos</p>
+        <button onClick={this.fetchDog}>Novo doguinho!</button>
+        <div>
+          <img src={this.state.data.message} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
